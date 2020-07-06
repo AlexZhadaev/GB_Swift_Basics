0ef3ea3ee340867a89d29328fcb1d2d9b2e677e7
AlexZhadaev <pvt.vatnik@gmail.com>
2020-07-07 00:32:26 +0300

    add homework

---
+++ 3l_AlexeyZhadaev/3l_AlexeyZhadaev/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  3l_AlexeyZhadaev
+//
+//  Created by Жадаев Алексей on 06.07.2020.
+//  Copyright © 2020 Жадаев Алексей. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
