be3a2f463ba51d2c21184bc77ce5446eba347464
AlexZhadaev <pvt.vatnik@gmail.com>
2020-07-07 23:03:48 +0300

    new comm

--- 3l_AlexeyZhadaev/3l_AlexeyZhadaev/main.swift
+++ 3l_AlexeyZhadaev/3l_AlexeyZhadaev/main.swift
@@ -1,12 +1,51 @@
 //
-//  main.swift
-//  3l_AlexeyZhadaev
-//
-//  Created by Жадаев Алексей on 06.07.2020.
-//  Copyright © 2020 Жадаев Алексей. All rights reserved.
-//
-
 import Foundation
+//1. Описать несколько структур – любой легковой автомобиль и любой грузовик.
+//2. Структуры должны содержать марку авто, год выпуска, объем багажника/кузова, запущен ли двигатель, открыты ли окна, заполненный объем багажника.
+//4. Добавить в структуры метод с одним аргументом типа перечисления, который будет менять свойства структуры в зависимости от действия - так и не получилось заставить mutating func подтягивать аргументы enum и изменять с их помощью filledTrunk. Получалась какая-то каша - то Editor placeholder in source file, то еще что.
 
-print("Hello, World!")
+struct aCar {
+    var brand: String
+    let year: Int
+    var trunkVolume: Int
+    var engStat: EngineStatus
+    var winStat: WindowStatus
+    var filledTrunk: Int
+    var trunkOper: TruckActions
+}
+
+struct carTruck {
+    var brand: String
+    let year: Int
+    var trunkVolume: Int
+    var engStat: EngineStatus
+    var winStat: WindowStatus
+    var filledTrunk: Int
+    var trunkOper: TruckActions
+    
+}
+
+
+//3. Описать перечисление с возможными действиями с автомобилем: запустить/заглушить двигатель, открыть/закрыть окна, погрузить/выгрузить из кузова/багажника груз определенного объема.
+enum EngineStatus {
+    case On, Off
+}
+enum WindowStatus {
+    case Open, Close
+}
+enum TruckActions: Int {
+case put = 100
+case unload = 4900
+}
+//fakecomment
+
+//5. Инициализировать несколько экземпляров структур. Применить к ним различные действия.
+var volga = aCar(brand: "Volga", year: 1999, trunkVolume: 500, engStat: .On, winStat: .Open, filledTrunk: 150, trunkOper: .put)
+
+var kamaz = carTruck(brand: "Kamaz", year: 1983, trunkVolume: 23700, engStat: .Off, winStat: .Close, filledTrunk: 19000, trunkOper: .unload)
+
+//6. Вывести значения свойств экземпляров в консоль.
+
+print(kamaz.brand, kamaz.year, kamaz.trunkVolume, kamaz.filledTrunk)
+print(volga.brand, volga.year, volga.trunkVolume, volga.filledTrunk)
 
